068942434bab3300c96b4b84bf058be34729f31d
MadhaviJayasinghe <lmadhavirj7512@gmail.com>
2022-05-07 10:40:57 +0530

    Modify store funds service

---
+++ src/main/java/com/dexlk/address/VO/ValidationResponse.java
@@ -0,0 +1,20 @@
+package com.dexlk.address.VO;
+
+public class ValidationResponse {
+    private String response;
+
+    public ValidationResponse() {
+    }
+
+    public ValidationResponse(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
